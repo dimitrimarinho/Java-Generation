c2052a17c7de376e67c5e51b0791870938ec1cb1
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2022-05-06 12:05:28 -0300

    Adicionando comentário

--- Resoluções/2 - Laços de Decisão/CategoriaIdade.java
+++ Resoluções/2 - Laços de Decisão/CategoriaIdade.java
@@ -2,6 +2,8 @@ package resolucoes;
 import java.util.Scanner;
 
 public class CategoriaIdade {
+	
+	/* Faça um programa que receba a idade de uma pessoa e mostre na saída em qual categoria ela se encontra */
 
 	public static void main(String[] args) {
 		
